99c96d69b9a1232c3bb8b12bd88eb9df1f884491
navyas211@gmail.com <navyas211@gmail.com>
2021-09-17 23:56:58 +0530

    [NAVYA]ADDED: Snake & Ladder Game Play With Single Player

--- src/com/navya/javaprogram/SnakeLadderGame.java
+++ src/com/navya/javaprogram/SnakeLadderGame.java
@@ -1,11 +1,18 @@
 package com.navya.javaprogram;
+/*
+ * Snake and Ladder game playe with single player at start position 0 .
+ */
 
-public class SnakeLadderGame {
+public class SnakeLadderGame
+{
+  public static void main(String[] args)
+ {
+    System.out.println("Welcome to Snake and Ladder Simulator problems ");
 
-	public static void main(String[] args)
-	{
-      
-		System.out.println("Welcome to Snake and Ladder Simulator problems ");
-
+    int player=1;
+    
+    int position=0;
+    
+    System.out.println("Player in the Position = " +position);
 	}
  }
