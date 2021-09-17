ba4f3cdd77054c2e7113b6e1b12b8a199bd9ffe2
navyas211@gmail.com <navyas211@gmail.com>
2021-09-17 23:38:17 +0530

    [NAVYA]ADDED: Welcome to Snake and Ladder Game

---
+++ src/com/navya/javaprogram/SnakeLadderGame.java
@@ -0,0 +1,11 @@
+package com.navya.javaprogram;
+
+public class SnakeLadderGame {
+
+	public static void main(String[] args)
+	{
+      
+		System.out.println("Welcome to Snake and Ladder Simulator problems ");
+
+	}
+ }
