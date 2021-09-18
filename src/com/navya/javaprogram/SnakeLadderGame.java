7ac03be4bf09af7398eae0153e9e935af992ceb5
navyas211@gmail.com <navyas211@gmail.com>
2021-09-18 19:16:45 +0530

    [NAVYA]ADDED: Play Rolls The Die to Get a Number Between 1 to 6

--- src/com/navya/javaprogram/SnakeLadderGame.java
+++ src/com/navya/javaprogram/SnakeLadderGame.java
@@ -1,18 +1,37 @@
 package com.navya.javaprogram;
+
+import java.util.Scanner;
+
 /*
- * Snake and Ladder game playe with single player at start position 0 .
+ * Snake and Ladder game play with single player at start position 0 .
+ * The player roll the die and get the number between 1 to 6 .
  */
 
 public class SnakeLadderGame
 {
+
+	//use method to roll die
+
+	static void rolldie() {
+		int min=1;
+		int max=6;
+		int Number= (int) Math.floor(Math.random()*(max-min)+min);
+		
+		System.out.println("Die = "+Number);
+	}
+	
   public static void main(String[] args)
  {
-    System.out.println("Welcome to Snake and Ladder Simulator problems ");
-
-    int player=1;
-    
+   
     int position=0;
-    
+   Scanner scanner = new Scanner(System.in);
+   
+   System.out.println("Enter how many players want to play");
+   int player=scanner.nextInt();
+   
     System.out.println("Player in the Position = " +position);
+    
+    //Function call
+    rolldie();
 	}
  }
