3f4fb06398cf99b2e6cb5f7d36fe87428e6ada9d
navyas211@gmail.com <navyas211@gmail.com>
2021-09-18 22:44:03 +0530

    [NAVYA]ADDED: Play then checks for a option They are No Play ladder or Snake

--- src/com/navya/javaprogram/SnakeLadderGame.java
+++ src/com/navya/javaprogram/SnakeLadderGame.java
@@ -1,10 +1,9 @@
 package com.navya.javaprogram;
 
-import java.util.Scanner;
-
 /*
- * Snake and Ladder game play with single player at start position 0 .
- * The player roll the die and get the number between 1 to 6 .
+ *1> Snake and Ladder game play with single player at start position 0 .
+ *2> The player roll the die and get the number between 1 to 6 .
+ *3> Check option they are no play, Ladder or Snake .
  */
 
 public class SnakeLadderGame
@@ -12,26 +11,46 @@ public class SnakeLadderGame
 
 	//use method to roll die
 
-	static void rolldie() {
+	static void rolldie()
+	{
 		int min=1;
 		int max=6;
-		int Number= (int) Math.floor(Math.random()*(max-min)+min);
+		int dieNumber= (int) Math.floor(Math.random()*(max-min)+min);
 		
-		System.out.println("Die = "+Number);
+		System.out.println("Die = "+dieNumber);
+	}
+	
+	static void checkPlayer(int check) 
+	{
+		// check they are no play, ladder or snake
+		switch (check)
+		{
+		case 0:
+			   System.out.println("They are no play");
+			   break;
+		case 1:
+			   System.out.println("Ladder");
+			   rolldie();
+			   break;
+		case 2:
+			   System.out.println("Snake");
+			   rolldie();
+			   break;
+		default:
+			    System.out.println("Some thing went wrong");
+		}
 	}
 	
   public static void main(String[] args)
  {
-   
     int position=0;
-   Scanner scanner = new Scanner(System.in);
-   
-   System.out.println("Enter how many players want to play");
-   int player=scanner.nextInt();
-   
+    int player = 1;
     System.out.println("Player in the Position = " +position);
     
-    //Function call
-    rolldie();
+    //Generate random number
+    int check = (int) Math.floor((Math.random()*10) % 3 );
+    
+    // static method call
+    checkPlayer(check);
 	}
  }
